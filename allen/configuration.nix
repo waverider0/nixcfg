a3c9cb0713c09b66a6cfe2454fc0ec6552d1d6f6
allen <waverider0@users.noreply.github.com>
2025-03-09 06:12:41 -0400

    brave

--- allen/configuration.nix
+++ allen/configuration.nix
@@ -32,6 +32,7 @@
         extraGroups = [ "networkmanager" "wheel" ];
         packages = (with pkgs; [
             binutils
+            brave
             clang
             direnv
             discord
@@ -70,13 +71,6 @@
     };
 
     programs = {
-        firefox.enable = true;
-        steam = {
-            enable = true;
-            remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-            dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-            localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-        };
         zsh = {
             enable = true;
             ohMyZsh.enable = true;
