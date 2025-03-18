723d06af620ab3f765f4c52e89dbcd37097238fd
allen <waverider0@users.noreply.github.com>
2025-03-17 20:34:13 -0400

    -

--- allen/dotfiles/nvim/lua/plugins.lua
+++ allen/dotfiles/nvim/lua/plugins.lua
@@ -28,11 +28,6 @@ require("lazy").setup({
             dependencies = { 'nvim-lua/plenary.nvim' },
         },
         {
-            "ThePrimeagen/harpoon",
-            branch = "harpoon2",
-            dependencies = { "nvim-lua/plenary.nvim" },
-        },
-        {
             "nvim-treesitter/nvim-treesitter",
             build = ":TSUpdate",
         },
@@ -60,19 +55,6 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-t>', builtin.find_files, {})
 vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>g', builtin.live_grep, {})
-
--- harpoon
-
-local harpoon = require("harpoon")
-harpoon:setup()
-
-vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-vim.keymap.set("n", "<leader>e", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
-vim.keymap.set("n", "<leader>1", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
-vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
 
 -- treesitter
 
