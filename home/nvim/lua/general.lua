c48885e96dab293b504214710cc03be4585fb78b
allen <waverider0@users.noreply.github.com>
2025-07-12 06:05:32 -0400

    ergo

--- home/nvim/lua/general.lua
+++ home/nvim/lua/general.lua
@@ -11,14 +11,15 @@ vim.opt.smartcase      = true
 vim.opt.swapfile       = false
 vim.opt.wrap           = false
 
-vim.keymap.set("i"           , "<C-c>"     , "<Esc>")
-vim.keymap.set("n"           , "Q"         , "<nop>")
-vim.keymap.set("n"           , "<C-c>"     , function() if vim.v.hlsearch == 1 then vim.cmd("nohlsearch") end end)
-vim.keymap.set("n"           , "<C-e>"     , "<nop>")
-vim.keymap.set({"n","v"}     , "<C-d>"     , "<C-d>zz")
-vim.keymap.set({"n","v"}     , "<C-u>"     , "<C-u>zz")
-vim.keymap.set({"n","v"}     , "<leader>e" , ":Ex<CR>")
-vim.keymap.set({"n","v","i"} , "<C-q>"     , function() vim.cmd(#vim.api.nvim_list_wins() == 1 and "q" or "bd") end)
+vim.keymap.set("i"           , "<C-c>"      , "<Esc>")
+vim.keymap.set("n"           , "Q"          , "<nop>")
+vim.keymap.set("n"           , "<C-c>"      , function() if vim.v.hlsearch == 1 then vim.cmd("nohlsearch") end end)
+vim.keymap.set("n"           , "<C-e>"      , "<nop>")
+vim.keymap.set({"n","v"}     , "<C-d>"      , "<C-d>zz")
+vim.keymap.set({"n","v"}     , "<C-u>"      , "<C-u>zz")
+vim.keymap.set({"n","v"}     , "<leader>dm" , function() vim.cmd("delm!") end)
+vim.keymap.set({"n","v"}     , "<leader>e"  , ":Ex<CR>")
+vim.keymap.set({"n","v","i"} , "<C-q>"      , function() vim.cmd(#vim.api.nvim_list_wins() == 1 and "q" or "bd") end)
 
 vim.cmd("autocmd FileType * setlocal formatoptions-=cro") -- disable automatic commenting on newline
 vim.cmd("autocmd FileType netrw setlocal syntax=netrw")
